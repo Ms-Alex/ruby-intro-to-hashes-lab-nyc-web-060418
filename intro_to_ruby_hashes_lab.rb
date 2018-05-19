02ec6ca25d97c4ddccea1723d024985c2b83f1b5
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-19 16:55:27 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	actor = {name: "Dwayne The Rock Johnson"}
+	actor = {name: "Dwayne The Rock Johnson", railroads: {}}
 end
 
 def monopoly_with_third_tier
